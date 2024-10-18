40d837a07bbacec2a14c7f9927a5aec90db9e369
Andres Zapata <afzar27@gmail.com>
2024-10-17 23:47:18 -0500

    init demo

---
+++ init.bash
@@ -0,0 +1,12 @@
+
+## Crear conectores
+curl -X POST -H "Content-Type: application/json" --data @source-connector.json http://localhost:8083/connectors
+curl -X POST -H "Content-Type: application/json" --data @sink-connector.json http://localhost:8084/connectors
+
+## Eliminar conectores
+
+## curl -X DELETE http://localhost:8083/connectors/postgres-source
+## curl -X DELETE http://localhost:8084/connectors/postgres-sink
+
+
+
